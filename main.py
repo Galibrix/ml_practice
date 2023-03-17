dd84fd64ed7487ac29c74cb7b584c3f0a0b5847c
galibrix <yaroslaw.gribanov@yandex.ru>
2023-03-17 16:26:07 +0300

    Function knn_classifier change the input data from pd.DataFrame to list[LabeledPoint] Added NamedTuple named points_by_gender A scatter plot is constructed The model was tested on split data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from typing import List, Tuple, TypeVar, NamedTuple
-from collections import Counter
+import random
+from typing import List, Tuple, TypeVar, NamedTuple, Dict
+from collections import Counter, defaultdict
 import matplotlib.pyplot as plt
 import math
 import pandas as pd
@@ -39,10 +40,10 @@ def distance(v: Vector, w: Vector) -> float:
     return math.sqrt(sum((v_i - w_i) ** 2 for v_i, w_i in zip(v, w)))
 
 
-def split_data(df: pd.DataFrame, prob: float) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    df.sample(frac=1)
-    prob = int(len(df) * prob)
-    return df.loc[:prob], data.loc[prob:]
+def split_data(data: list[X], prob: float) -> Tuple[list[X], list[X]]:
+    random.shuffle(data)
+    prob = int(len(data) * prob)
+    return data[:prob], data[prob:]
 
 # print(split_data(males, 0.7))
 
@@ -55,18 +56,70 @@ def majority_vote(genders: list[str]) -> str:
     else:
         majority_vote(genders[:-1])
 
-class Labeled_point(NamedTuple):
+class LabeledPoint(NamedTuple):
     gender: str
     point: Vector
 
 
-def knn_classifier(k: int, labeled_points: pd.DataFrame, new_point: Vector) -> str:
-    labeled_points = [Labeled_point(gender, [income, exp])
-                      for income, exp, gender in zip(labeled_points['Annual Income ($)'],
-                                                     labeled_points['Work Experience'],
-                                                     labeled_points['Gender'])]
+def knn_classifier(k: int, labeled_points: list[LabeledPoint], new_point: Vector) -> str:
     by_distance = sorted(labeled_points, key=lambda lp: distance(lp.point, new_point))
     k_nearest_genders = [lp.gender for lp in by_distance[:k]]
     return majority_vote(k_nearest_genders)
 
-print(knn_classifier(5, data, [150, 6]))
+# print(knn_classifier(5, data, [150, 6]))
+
+points_by_gender: Dict[str, list[Vector]] = defaultdict(list)
+
+labeled_points = [LabeledPoint(gender, [income, exp])
+                  for income, exp, gender in zip(data['Annual Income ($)'],
+                                                 data['Work Experience'],
+                                                 data['Gender'])]
+
+for customer in labeled_points:
+    points_by_gender[customer.gender].append(customer.point)
+print(points_by_gender)
+
+metrics = ['Work experience', 'Annual Income ($)']
+marks = ['x', '+']
+
+fig, ax = plt.subplots(2, 2)
+
+for i in range(len(metrics)):
+    for j in range(len(metrics)):
+
+        if i != j:
+            ax[i][j].set_title(f'{metrics[i]} to {metrics[j]}', fontsize=8)
+            ax[i][j].set_yticks([])
+            ax[i][j].set_xticks([])
+            for mark, (gender, points) in zip(marks, points_by_gender.items()):
+                xs = [point[i] for point in points]
+                ys = [point[j] for point in points]
+                ax[i][j].scatter(xs, ys, marker=mark, label=gender, alpha=0.4)
+
+        else:
+            ax[i][j].annotate('Series' + str(j), (0.5, 0.5), xycoords='axes fraction', va='center', ha='center')
+
+        if i < len(metrics)-1: ax[i][j].xaxis.set_visible(False)
+        if j > 0: ax[i][j].yaxis.set_visible(False)
+
+ax[0][len(metrics)-1].legend(loc='upper right')
+plt.show()
+
+
+confusion_matrix: Dict[Tuple[str, str], int] = defaultdict(int)
+num_correct = 0
+
+gender_train, gender_test = split_data(labeled_points, 0.7)
+
+for customer in gender_test:
+    predicted = knn_classifier(5, gender_train, customer.point)
+    actual = customer.gender
+
+    if predicted == actual:
+        num_correct += 1
+
+    confusion_matrix[(predicted, actual)] += 1
+pct_correct = num_correct / len(gender_test)
+print(pct_correct, confusion_matrix)
+
+print(knn_classifier(5, labeled_points, [180, 0]))
